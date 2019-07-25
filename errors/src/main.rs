85db1f2bc21b6218a99b9b8cedd8a4f68ca9d38a
chadrm9 <chad.r.milburn@gmail.com>
2019-07-25 17:08:48 -0400

    1.4 nicer errors

---
+++ errors/src/main.rs
@@ -0,0 +1,9 @@
+use failure::ResultExt;
+use exitfailure::ExitFailure;
+
+fn main() -> Result<(), ExitFailure> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path).with_context(|_| format!("could not read file `{}`", path))?;
+    println!("File contents: {}", content);
+    Ok(())
+}
